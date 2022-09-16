0eb6d32c54c221013b7fee4647e65a863d918895
atmcmustafa <mustafa_atmaca57@hotmail.com>
2022-09-16 18:32:56 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      kumbhSans: 'Kumbh Sans',
+    },
+    container: {
+      center: 'true',
+    },
+    colors: {
+      white: '#fff',
+      darkCyan: 'hsl(185, 75%, 39%)',
+      veryDarkblue: 'hsl(229, 23%, 23%)',
+      darkGrayishBlue: 'hsl(227, 10%, 46%)',
+      darkGray: 'hsl(0, 0%, 59%)',
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
